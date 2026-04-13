cdb84e6b18edb0d198a8e6065d4889975e893929
nishi147 <nsk110761@gmail.com>
2026-04-14 01:26:28 +0530

    5000 port

--- backend/server.js
+++ backend/server.js
@@ -36,10 +36,16 @@ app.use((err, req, res, next) => {
   });
 });
 
+// if (process.env.NODE_ENV !== 'production') {
+//   const PORT = process.env.PORT || 5000;
+//   app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+//   });
+// }
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
 
 module.exports = app;
