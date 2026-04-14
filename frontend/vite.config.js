32d870bc5cb56958b483f05e187c0c6e632ea8e4
nishi147 <nsk110761@gmail.com>
2026-04-15 01:32:05 +0530

    coursess

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://manshuportfolio.onrender.com',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
         timeout: 600000, // 10 minutes
